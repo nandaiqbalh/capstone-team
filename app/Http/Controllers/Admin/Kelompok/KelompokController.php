6bfa0da8d8ab664001a83ca6e4e2e90f32ac6563
LAPTOP-QU77DM4U\imamg <imam.gh98@gmail.com>
2023-05-13 20:27:40 +0700

    kelompok

---
+++ app/Http/Controllers/Admin/Kelompok/KelompokController.php
@@ -0,0 +1,272 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Kelompok;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Controllers\Admin\BaseController;
+use App\Models\Admin\Mahasiswa\MahasiswaModel;
+use Illuminate\Support\Facades\Hash;
+
+
+class KelompokController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    //coba comment
+
+    public function index()
+    {
+        // authorize
+        MahasiswaModel::authorize('R');
+        // dd(MahasiswaModel::getData());
+
+        // get data with pagination
+        $rs_kelompok = MahasiswaModel::getDataWithPagination();
+        // data
+        $data = ['rs_kelompok' => $rs_kelompok];
+        // view
+        return view('admin.kelompok.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function addMahasiswa()
+    {
+        // authorize
+        MahasiswaModel::authorize('C');
+
+        // view
+        return view('admin.mahasiswa.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function addMahasiswaProcess(Request $request)
+    {
+
+        // authorize
+        MahasiswaModel::authorize('C');
+
+        // Validate & auto redirect when fail
+        $rules = [
+            'nama' => 'required',
+            "nim" => 'required',
+            "angkatan" => 'required',
+            "ipk" => 'required',
+            "sks" => 'required',
+            "alamat" => 'required',
+        ];
+        $this->validate($request, $rules);
+
+
+        // params
+        // default passwordnya mahasiswa123
+        $user_id = MahasiswaModel::makeMicrotimeID();
+        $params = [
+            'user_id' => $user_id,
+            'user_name' => $request->nama,
+            "nomor_induk" => $request->nim,
+            "angkatan" => $request->angkatan,
+            "ipk" => $request->ipk,
+            "sks" => $request->sks,
+            'user_password' => Hash::make('mahasiswa123'),
+            "alamat" => $request->alamat,
+            'created_by'   => Auth::user()->user_id,
+            'created_date'  => date('Y-m-d H:i:s')
+        ];
+
+        // process
+        $insert_mahasiswa = MahasiswaModel::insertmahasiswa($params);
+        if ($insert_mahasiswa) {
+            $params2 = [
+                'user_id' => $user_id,
+                'role_id' => '03'
+            ];
+            MahasiswaModel::insertrole($params2);
+
+            // flash message
+            session()->flash('success', 'Data berhasil disimpan.');
+            return redirect('/admin/mahasiswa');
+        } else {
+            // flash message
+            session()->flash('danger', 'Data gagal disimpan.');
+            return redirect('/admin/settings/contoh-halaman/add')->withInput();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function detailMahasiswa($user_id)
+    {
+        // authorize
+        MahasiswaModel::authorize('R');
+
+        // get data with pagination
+        $mahasiswa = MahasiswaModel::getDataById($user_id);
+
+        // check
+        if (empty($mahasiswa)) {
+            // flash message
+            session()->flash('danger', 'Data tidak ditemukan.');
+            return redirect('/admin/mahasiswa');
+        }
+
+        // data
+        $data = ['mahasiswa' => $mahasiswa];
+
+        // view
+        return view('admin.mahasiswa.detail', $data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function editMahasiswa($user_id)
+    {
+        // authorize
+        MahasiswaModel::authorize('U');
+
+        // get data 
+        $mahasiswa = MahasiswaModel::getDataById($user_id);
+
+        // check
+        if (empty($mahasiswa)) {
+            // flash message
+            session()->flash('danger', 'Data tidak ditemukan.');
+            return redirect('/admin/mahasiswa');
+        }
+
+        // data
+        $data = ['mahasiswa' => $mahasiswa];
+
+        // view
+        return view('admin.mahasiswa.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function editMahasiswaProcess(Request $request)
+    {
+        // authorize
+        MahasiswaModel::authorize('U');
+
+        // Validate & auto redirect when fail
+        $rules = [
+            'nama' => 'required',
+            "nim" => 'required',
+            "angkatan" => 'required',
+            "ipk" => 'required',
+            "sks" => 'required',
+            "alamat" => 'required',
+        ];
+        $this->validate($request, $rules);
+
+        // params
+        $params = [
+            'user_name' => $request->nama,
+            "nomor_induk" => $request->nim,
+            "angkatan" => $request->angkatan,
+            "ipk" => $request->ipk,
+            "sks" => $request->sks,
+            "alamat" => $request->alamat,
+            'modified_by'   => Auth::user()->user_id,
+            'modified_date'  => date('Y-m-d H:i:s')
+        ];
+
+        // process
+        if (MahasiswaModel::update($request->user_id, $params)) {
+            // flash message
+            session()->flash('success', 'Data berhasil disimpan.');
+            return redirect('/admin/mahasiswa');
+        } else {
+            // flash message
+            session()->flash('danger', 'Data gagal disimpan.');
+            return redirect('/admin/mahasiswa/edit/' . $request->user_id);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function deleteMahasiswaProcess($user_id)
+    {
+        // authorize
+        MahasiswaModel::authorize('D');
+
+        // get data
+        $mahasiswa = MahasiswaModel::getDataById($user_id);
+
+        // if exist
+        if (!empty($mahasiswa)) {
+            // process
+            if (MahasiswaModel::delete($user_id)) {
+                // flash message
+                session()->flash('success', 'Data berhasil dihapus.');
+                return redirect('/admin/mahasiswa');
+            } else {
+                // flash message
+                session()->flash('danger', 'Data gagal dihapus.');
+                return redirect('/admin/settings/contoh-halaman');
+            }
+        } else {
+            // flash message
+            session()->flash('danger', 'Data tidak ditemukan.');
+            return redirect('/admin/settings/contoh-halaman');
+        }
+    }
+
+    /**
+     * Search data.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function search(Request $request)
+    {
+        // authorize
+        MahasiswaModel::authorize('R');
+
+        // data request
+        $nama = $request->nama;
+
+        // new search or reset
+        if ($request->action == 'search') {
+            // get data with pagination
+            $rs_ch = MahasiswaModel::getDataSearch($nama);
+            // data
+            $data = ['rs_ch' => $rs_ch, 'nama' => $nama];
+            // view
+            return view('admin.settings.contoh-halaman.index', $data);
+        } else {
+            return redirect('/admin/settings/contoh-halaman');
+        }
+    }
+}
