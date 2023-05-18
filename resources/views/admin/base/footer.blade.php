8429bf5ff0630b0dac46325aa0d0a2a82a13abee
lita muflikha <mufel27@gmail.com>
2023-05-18 22:54:09 +0700

    puuter
    bdeh

--- resources/views/admin/base/footer.blade.php
+++ resources/views/admin/base/footer.blade.php
@@ -1,7 +1,6 @@
             <footer class="content-footer footer bg-footer-theme">
                 <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
                     <div class="mb-2 mb-md-0"> Copyright © {{date('Y')}} <strong>Teknik Komputer Undip</strong> </div>
-                    <div class="mb-2 mb-md-0"> Copyright © {{date('Y')}} <strong>Teknik Komputer</strong> </div>
                     <div>
                         <small>
                             {{$app_version}}
