0fbe4c1a1b933c2ad6a9ba31b0b8c4169fecac1d
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2024-01-30 23:53:48 +0700

    API Broadcast with image

--- app/Http/Controllers/API/V1/Mahasiswa/Broadcast/ApiBroadcastController.php
+++ app/Http/Controllers/API/V1/Mahasiswa/Broadcast/ApiBroadcastController.php
@@ -15,6 +15,28 @@ class ApiBroadcastController extends Controller
             // Get data with pagination
             $rs_broadcast = ApiBroadcastModel::getDataWithPagination();
 
+            foreach ($rs_broadcast as $key =>$broadcast ) {
+
+                $storagePath = null;
+
+                // dd( $broadcast);
+                if ($broadcast->broadcast_image_name != "" && $broadcast->broadcast_image_name != null) {
+                    $storagePath = public_path($broadcast->broadcast_image_path . $broadcast->broadcast_image_name);
+
+                    if (file_exists($storagePath)) {
+                        $base64Image = base64_encode(file_get_contents($storagePath));
+                        $rs_broadcast[$key]->broadcast_image_path = $base64Image;
+                    } else {
+                        $base64Image = base64_encode(file_get_contents(public_path('img/broadcast/default_broadcast.png')));
+                        $rs_broadcast[$key]->broadcast_image_path = $base64Image;
+                    }
+                } else {
+                    $base64Image = base64_encode(file_get_contents(public_path('img/broadcast/default_broadcast.png')));
+                    $rs_broadcast[$key]->broadcast_image_name = $base64Image;
+                }
+
+            }
+
             // Return JSON response with successful status
             $response = [
                 'status' => true,
@@ -40,6 +62,28 @@ class ApiBroadcastController extends Controller
             // Get data with pagination
             $rs_broadcast = ApiBroadcastModel::getDataWithHomePagination();
 
+            foreach ($rs_broadcast as $key =>$broadcast ) {
+
+                $storagePath = null;
+
+                // dd( $broadcast);
+                if ($broadcast->broadcast_image_name != "" && $broadcast->broadcast_image_name != null) {
+                    $storagePath = public_path($broadcast->broadcast_image_path . $broadcast->broadcast_image_name);
+
+                    if (file_exists($storagePath)) {
+                        $base64Image = base64_encode(file_get_contents($storagePath));
+                        $rs_broadcast[$key]->broadcast_image_path = $base64Image;
+                    } else {
+                        $base64Image = base64_encode(file_get_contents(public_path('img/broadcast/default_broadcast.png')));
+                        $rs_broadcast[$key]->broadcast_image_path = $base64Image;
+                    }
+                } else {
+                    $base64Image = base64_encode(file_get_contents(public_path('img/broadcast/default_broadcast.png')));
+                    $rs_broadcast[$key]->broadcast_image_name = $base64Image;
+                }
+
+            }
+
             // Return JSON response with successful status
             $response = [
                 'status' => true,
@@ -58,6 +102,9 @@ class ApiBroadcastController extends Controller
             return response()->json($response); // 500 Internal Server Error
         }
     }
+
+
+
 
    // New method for API endpoint
    public function detailBroadcastApi(Request $request)
